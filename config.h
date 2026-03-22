7747474c298c1450bd47b70aacfd736b9ecc9cd3
Your Name <you@example.com>
2026-03-21 18:46:23 -0600

    update

--- config.h
+++ config.h
@@ -10,7 +10,7 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#000000", "#bd93f9" },
+	[SchemeSel] = { "#000000", "#cba6f7" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 
